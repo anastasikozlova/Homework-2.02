80964e504742dbedbacfdd65619b088ac929c25b
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-04-23 16:19:49 +0200

    Initial Commit

---
+++ Homework 2.02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.02
+//
+//  Created by Анастасия Козлова on 23.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
