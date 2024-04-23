886483031e0a25b8cd64bdd09558d8a3dea556c1
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-04-23 17:25:42 +0200

    Rounded up the button and views

--- Homework 2.02/ViewController.swift
+++ Homework 2.02/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greenColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        greenColorView.layer.cornerRadius = 50
+        yellowColorView.layer.cornerRadius = 50
+        redColorView.layer.cornerRadius = 50
     }
 
 
