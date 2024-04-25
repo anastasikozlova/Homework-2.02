bb6ac4f8d77796835c1a57e108de0e1aee881377
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-04-25 17:21:35 +0200

    Setup light switching

--- Homework 2.02/ViewController.swift
+++ Homework 2.02/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var greenColorView: UIView!
     @IBOutlet var yellowColorView: UIView!
     @IBOutlet var redColorView: UIView!
     @IBOutlet var startButton: UIButton!
+    
+    var currentLightIndex = 0
+    var isLightOn = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +24,37 @@ class ViewController: UIViewController {
         greenColorView.layer.cornerRadius = 50
         yellowColorView.layer.cornerRadius = 50
         redColorView.layer.cornerRadius = 50
+        
+        greenColorView.alpha = 0.3
+        yellowColorView.alpha = 0.3
+        redColorView.alpha = 0.3
     }
-
-
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        if !isLightOn {
+            sender.setTitle("NEXT", for: .normal)
+            isLightOn = true
+        }
+        changeLight()
+    }
+    
+    func changeLight() {
+        switch currentLightIndex {
+        case 0:
+            redColorView.alpha = 1
+            yellowColorView.alpha = 0.3
+            greenColorView.alpha = 0.3
+        case 1:
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 1
+            greenColorView.alpha = 0.3
+        case 2:
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 0.3
+            greenColorView.alpha = 1
+        default:
+            break
+        }
+        currentLightIndex = (currentLightIndex + 1) % 3
+    }
 }
-
